4c2cbddcd933cc8598d2c8e94e307fabdc18e59b
GopOwl <viogarden16@gmail.com>
2022-01-06 16:02:14 +0900

    b9498

---
+++ baekjoon_단계별로_풀어보기/baekjoon입출력과_사칙연산/boj9498/시험 성적.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(__dirname + '/input.txt').toString().split('\n');
+
+// input = input.map(value => +value);
+
+solution(+input);
+console.log(typeof input)
+
+function solution(a) {
+    if (a >= 90) {
+        console.log('A');
+    }
+    else if (a >= 80) {
+        console.log('B');
+    }
+    else if (a >= 70) {
+        console.log('C');
+    }
+    else if (a >= 60) {
+        console.log('D');
+    }
+    else {
+        console.log('F');
+    }
+
+}
