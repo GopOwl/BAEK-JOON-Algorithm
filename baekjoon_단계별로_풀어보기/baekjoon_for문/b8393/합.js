307ca2ecc273447d557755a09baf3abe5e526d90
GopOwl <viogarden16@gmail.com>
2022-01-07 23:22:09 +0900

    b8393
    백준 문제 8393번 코드

---
+++ baekjoon_단계별로_풀어보기/baekjoon_for문/b8393/합.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(__dirname + '/input.txt').toString().split('\n');
+
+solution(+input[0]);
+
+function solution(n) {
+    let j = 0;
+    for (let i = 1; i <= n; i++) {
+        j = j+i;
+    }
+    console.log(j)
+}
