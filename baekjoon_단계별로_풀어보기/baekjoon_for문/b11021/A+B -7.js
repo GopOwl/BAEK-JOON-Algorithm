78caf1722389afc008d9c19e1ed16dd6bee1a837
GopOwl <viogarden16@gmail.com>
2022-01-17 19:22:58 +0900

    b11021
    백준 문제 11021 코드

---
+++ baekjoon_단계별로_풀어보기/baekjoon_for문/b11021/A+B -7.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(__dirname + '/input.txt').toString().split('\n');
+
+const testcaseArray = [];
+for (let i = 1; i<= +input[0]; i++) {
+    testcaseArray.push(input[i].split(' ').map(value => +value));
+}
+
+solution(+input[0],testcaseArray);
+
+function solution(T,testcaseArray) {
+    for (let i = 0; i<T; i++) {
+        const A = testcaseArray[i][0];
+        const B = testcaseArray[i][1];
+        console.log('Case #' + (i+1) + ':', (A+B));
+    }
+}
