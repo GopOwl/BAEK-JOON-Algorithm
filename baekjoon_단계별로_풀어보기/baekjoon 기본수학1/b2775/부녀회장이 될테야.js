fb48298857c80d9dcad406c4c6483f9c8eeb9647
GopOwl <viogarden16@gmail.com>
2022-03-14 18:17:44 +0900

    b2775
    백준 2775번 문제 코드

---
+++ baekjoon_단계별로_풀어보기/baekjoon 기본수학1/b2775/부녀회장이 될테야.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(__dirname + '/input.txt').toString().split('\n');
+
+const T = Number(input.shift());
+
+solution(T);
+
+function solution(T) {
+    for (let i = 0; i<T; i++) {
+        const k = +input.shift();
+        const n = +input.shift();
+        const house = Array.from(Array(k + 1), () => Array(n+1).fill(0));
+        console.log(house)
+        for (let i = 1; i <= n; i++) {
+            house[0][i] = i;
+        }
+        for (let i = 1; i <= k; i++) {
+            for (let j = 1; j <= n; j++) {
+                house[i][j] = house[i - 1][j] + house[i][j - 1];
+            }
+        }
+        console.log(house[k][n]);
+    }
+ 
+}
