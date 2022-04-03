53516be793f966c17ffd68be85e16e29524fc4e7
GopOwl <viogarden16@gmail.com>
2022-04-03 14:38:13 +0900

    b1929 다른풀이
    백준 1929번 에라스토텔레스의 체

---
+++ baekjoon_단계별로_풀어보기/baekjoon_기본 수학 2/b1929/소수 구하기(다른풀이).js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(__dirname + '/input.txt').toString().split('\n');
+
+input = input[0].split(' ').map(value => +value);
+
+solution(input[0], input[1]);
+
+function solution(M, N) {
+    let isPrimeNum = Array(N+1).fill(true);
+    isPrimeNum[0] = isPrimeNum[1] = false;
+    for (let i=2; i<=Math.ceil(Math.sqrt(N)); i++) {
+        if(isPrimeNum[i]) {
+            let m = 2;
+            while(i*m <= N) {
+                isPrimeNum[i*m] = false;
+                m++;
+            }
+        }
+    }
+    const result = [];
+    for(let i=M; i<=N; i++) {
+        if(isPrimeNum[i]) {
+            result.push(i);
+        }
+    }
+    console.log(result.join('\n'));
+}
