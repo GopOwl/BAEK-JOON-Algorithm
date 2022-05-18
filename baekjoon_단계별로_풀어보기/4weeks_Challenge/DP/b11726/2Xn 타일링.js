4bf47aa8f697e2dbf4b10cfb991e86ee1ad70f22
GopOwl <viogarden16@gmail.com>
2022-05-19 06:36:34 +0900

    b11726
    백준 4주 챌린지 11726번 풀이

---
+++ baekjoon_단계별로_풀어보기/4weeks_Challenge/DP/b11726/2Xn 타일링.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(__dirname + '/input.txt').toString().trim().split('\n');
+
+solution(+input[0]);
+
+function solution(n) {
+    const DP = new Array(n + 1).fill(0);
+    DP[1] = 1;
+    DP[2] = 2;
+    for (let i = 3; i <= n; i++) {
+        DP[i] = (DP[i - 1] + DP[i - 2]) % 10007;
+    }
+    console.log(DP[n]);
+}
